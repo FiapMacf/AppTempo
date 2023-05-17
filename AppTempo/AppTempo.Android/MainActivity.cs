6ccd4eb3e4f48304696ebbca880d1845a07fcf41
Fred Vieira <frederico.fv07@gmail.com>
2023-05-16 22:55:15 -0300

    feat: adding permissions

--- AppTempo/AppTempo.Android/MainActivity.cs
+++ AppTempo/AppTempo.Android/MainActivity.cs
@@ -1,8 +1,12 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Support.V4.App;
 using Prism;
 using Prism.Ioc;
+using System;
+using System.Collections.Generic;
 
 namespace AppTempo.Droid
 {
@@ -10,24 +14,54 @@ namespace AppTempo.Droid
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private static int MY_REQUEST_CODE = 100;
+        private bool loadApplication = false;
+
         protected override void OnCreate(Bundle savedInstanceState)
-                                                                                                                                                                                                                                   {
+        {
+
+            var permissions = new List<String>();
+
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Permission.Granted)
+            {
+                permissions.Add(Manifest.Permission.AccessFineLocation);
+            }
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Permission.Granted)
+            {
+                permissions.Add(Manifest.Permission.AccessCoarseLocation);
+            }
+
+            if (permissions.Count > 0)
+            {
+                loadApplication = true;
+                ActivityCompat.RequestPermissions(this, permissions.ToArray(), MY_REQUEST_CODE);
+            }
+
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
 
-            
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
-            LoadApplication(new App(new AndroidInitializer()));
+
+            if (permissions.Count == 0)
+                LoadApplication(new App(new AndroidInitializer()));
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (loadApplication)
+            {
+                LoadApplication(new App(new AndroidInitializer()));
+                loadApplication = false;
+            }
         }
     }
 
